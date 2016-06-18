6ab9d0c29215aa7325e658e714996807daad88fa
Michael Jørgensen <michael.finn.jorgensen@gmail.com>
2016-06-18 10:31:47 +0200

    Checkin time.

--- sudoku/sudoku.rb
+++ sudoku/sudoku.rb
@@ -1,23 +1,27 @@
 
-problems = []
-problem = []
-dat = File.read('1.txt') # Read entire file
-row_num = 0
+def read_problems(fname)
+    problems = []
+    problem = []
+    dat = File.read(fname) # Read entire file
+    row_num = 0
 
-dat.each_line { |l| 
-    p l
-    if row_num == 0
-        problem_num = l.match('Grid (..)')[1]
-        problem = []
-    else
-        row = l.chomp.split('')
-        problem << row
-    end
+    dat.each_line { |l| 
+        if row_num == 0
+            problem_num = l.match('Grid (..)')[1]
+            problem = []
+        else
+            row = l.chomp.split('')
+            problem << row
+        end
 
-    row_num += 1
-    if row_num > 9
-        problems << problem
-        row_num = 0
-    end
-}
+        row_num += 1
+        if row_num > 9
+            problems << problem
+            row_num = 0
+        end
+    }
+    return problems
+end
+
+problems = read_problems('2.txt')
 p problems
