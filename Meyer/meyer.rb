98a6e17c579f12ad5464ef9a8a6b8dd8aa1465bb
Michael Jørgensen <michael.finn.jorgensen@gmail.com>
2016-06-25 23:07:56 +0200

    Refactoring

--- Meyer/meyer.rb
+++ Meyer/meyer.rb
@@ -13,62 +13,107 @@ def gen_slag
 	return slag
 end
 
+# Dette er den centrale bot, som udregner computerens træk.
+# "melding" er den aktuelle melding. Hvis det er første slag, så er melding = ""
+# "nyt_slag" bruges kun, hvis bot'en ryster terningerne.
+# NB: Det er muligt for bot'en at snyde, ved at kigge på nyt_slag, men det er forbudt!
+def comp_melding(melding, nyt_slag)
+	# Det er min tur!
+	if melding == "" # Tom streng betyder, at det er første slag
+		# Find bare en tilfældig melding, uafhængig af hvad terningerne viser.
+		# TBD1 : Meldingen må godt tage højde for, hvad terningerne viser.
+		melding = $meyer[rand($meyer.size)]
+	elsif rand < 0.5 # TBD2 : Om vi vælger at stole på spilleren kan godt afhænge af den seneste melding.
+		# Vi stoler på spilleren og ryster terningerne.
+		if $meyer.index(nyt_slag) > $meyer.index(melding)
+			# Øv. Mit nye slag er mindre end det sidst meldte.
+			# Jeg må lyve.
+			# Jeg vælger en tilfældig af de mulige meldinger.
+			antal_lovlige_meldinger = $meyer.index(melding) + 1
+			melding = $meyer[rand(antal_lovlige_meldinger)]
+		else
+			# Jeg melder bare det mine terninger viser.
+			melding = nyt_slag # TBD3 : Vores nye melding må godt være både bedre og dårligere en vores terninger.
+		end
+	else
+		# Vi stoler ikke på spilleren, så vi løfter terningerne
+		melding = ""
+	end
+	
+	return melding
+end
+
+def spiller_melding(melding, nyt_slag)
+
+	aktion = "ryst"
+	if melding != "" # Tom streng betyder, at det er første slag
+		print "\nDu kan enten skrive løft eller ryst\n"
+		aktion = gets.chomp.encode(Encoding::UTF_8)
+	end
+
+	if aktion == "løft"
+		return ""
+	elsif aktion == "ryst"
+		print "\nDu ryster\n"
+		print "Terningerne viser nu #{nyt_slag}. Hvad vil du melde?\n"
+
+		lovlig_melding = false
+		begin
+			ny_melding = gets.chomp
+			if $meyer.include?(ny_melding)
+				print "\nDu melder #{ny_melding}\n"
+				if melding != "" and $meyer.index(ny_melding) > $meyer.index(melding)
+					print "Det er ikke lovligt. Prøv igen.\n"
+				else
+					lovlig_melding = true
+				end
+			else
+				print "Det forstår jeg ikke. Prøv igen.\n"
+			end
+		end while not lovlig_melding
+		melding = ny_melding
+	end
+	
+	return melding
+end
+
 def spil_en_runde(min_tur)
     print "=====================\n\n"
 
-    melding = "" 
+    melding = ""
+	aktuelt_slag = ""
     vinder = 0
     while vinder == 0
 
         if min_tur
-            # Det er min tur nu!
-            if melding == "" # Tom streng betyder, at det er første slag
+			# Det er computerens tur!
+			nyt_slag = gen_slag # Vi genererer et nyt slag. Det bruges, hvis bot'en ryster terningerne
+			ny_melding = comp_melding(melding, nyt_slag)
+			if ny_melding == ""
+				print "Jeg løfter.\n"
+				print "Terningerne viser #{aktuelt_slag}\n"
+				print "Du havde meldt #{melding}\n"
+
+				if $meyer.index(aktuelt_slag) > $meyer.index(melding)
+					print "Du har tabt\n\n"
+					vinder = 1
+				else
+					print "Jeg har tabt\n\n"
+					vinder = -1
+				end
+			else
+				aktuelt_slag = nyt_slag
+				melding = ny_melding
                 print "Jeg ryster terningerne og kigger.\n"
-                aktuelt_slag = gen_slag
-
-                # Find bare en tilfældig melding, uafhængig af hvad terningerne viser.
-                melding = $meyer[rand($meyer.size)]
                 print "Jeg melder #{melding}.\n"
-            elsif rand < 0.5 # Vi stoler kun nogle gange på spilleren
-                # Ryst
-                print "Jeg ryster terningerne og kigger.\n"
-                aktuelt_slag = gen_slag
-
-                if $meyer.index(aktuelt_slag) > $meyer.index(melding)
-                    # Øv. Mit nye slag er mindre end det sidst meldte.
-                    # Jeg må lyve.
-                    antal_lovlige_meldinger = $meyer.index(melding) + 1
-                    melding = $meyer[rand(antal_lovlige_meldinger)]
-                else
-                    # Jeg melder bare det mine terninger viser.
-                    melding = aktuelt_slag
-                end
-                print "Jeg melder #{melding}.\n"
-            else
-                # Løft
-                print "Jeg løfter.\n"
-                print "Terningerne viser #{aktuelt_slag}\n"
-                print "Du havde meldt #{melding}\n"
-
-                if $meyer.index(aktuelt_slag) > $meyer.index(melding)
-                    print "Du har tabt\n\n"
-                    vinder = 1
-                else
-                    print "Jeg har tabt\n\n"
-                    vinder = -1
-                end
-            end
-
+			end
+			
         else
             # Det er spillerens tur!
-
-            aktion = "ryst"
-            if melding != "" # Tom streng betyder, at det er første slag
-                print "\nDu kan enten skrive løft eller ryst\n"
-                aktion = gets.chomp.encode(Encoding::UTF_8)
-            end
-
-            if aktion == "løft"
+			nyt_slag = gen_slag # Vi genererer et nyt slag. Det bruges, hvis bot'en ryster terningerne
+			ny_melding = spiller_melding(melding, nyt_slag)
+			
+			if ny_melding == ""
                 print "\nDu løfter\n"
                 print "Terningerne viser #{aktuelt_slag}\n"
                 if $meyer.index(aktuelt_slag) > $meyer.index(melding)
@@ -78,28 +123,10 @@ def spil_en_runde(min_tur)
                     print "Du har tabt\n\n"
                     vinder = 1
                 end
-            elsif aktion == "ryst"
-                print "\nDu ryster\n"
-                aktuelt_slag = gen_slag
-                print "Terningerne viser nu #{aktuelt_slag}. Hvad vil du melde?\n"
-
-                lovlig_melding = false
-                begin
-                    ny_melding = gets.chomp
-                    if $meyer.include?(ny_melding)
-                        print "\nDu melder #{ny_melding}\n"
-                        if melding != "" and $meyer.index(ny_melding) > $meyer.index(melding)
-                            print "Det er ikke lovligt. Prøv igen.\n"
-                        else
-                            lovlig_melding = true
-                        end
-                    else
-                        print "Det forstår jeg ikke. Prøv igen.\n"
-                    end
-                end while not lovlig_melding
-                melding = ny_melding
-
-            end
+			else
+				aktuelt_slag = nyt_slag
+				melding = ny_melding
+			end
         end
         
         min_tur = not(min_tur)
