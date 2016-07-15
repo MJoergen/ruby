9a2abe1088bb140322cb9e22fcee8cea182148af
Michael Jørgensen <michael.finn.jorgensen@gmail.com>
2016-07-15 21:21:04 +0200

    First version.

---
+++ tennis/wall.rb
@@ -0,0 +1,19 @@
+class Wall
+  attr_reader :width
+  attr_reader :height
+  
+  def initialize(window)
+    @window = window
+    @image = Gosu::Image.new(@window, "media/bjælke.png", false)
+    @width  = @image.width
+    @height = @image.height
+
+	@x = @window.width/2 - @width/2
+	@y = @window.height - @height
+  end
+  
+  def draw
+	@image.draw(@x, @y, 2)
+  end
+  
+end
