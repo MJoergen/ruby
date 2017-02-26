e458588ec87f7c84c3e2e6daa8f09fc743df7c80
Michael Jørgensen <michael.finn.jorgensen@gmail.com>
2017-02-26 21:42:18 +0100

    Checkin time.

--- nottt/nottt.rb
+++ nottt/nottt.rb
@@ -19,6 +19,8 @@ class Window < Gosu::Window
        
         @thinking = false
         @game_over = false
+        
+        @last_move = [0, 0]
     end
 
     def needs_cursor?
@@ -63,9 +65,35 @@ class Window < Gosu::Window
 
     def update
         if @thinking
+            # It is our turn
             if is_dead?
+                # We won!
                 @game_over = true
             else
+                # Look through all empty square
+                moves = []
+                legal = []
+                (0..@size-1).each {|row|
+                    (0..@size-1).each {|col|
+                        if not @board[row][col]
+                            legal << [row, col]
+                            @board[row][col] = true
+                            if not is_dead?
+                                moves << [row, col]
+                            end
+                            @board[row][col] = false
+                        end
+                    }
+                }
+                if moves.size > 0
+                    row, col = (moves.shuffle)[0]
+                else
+                    row, col = (legal.shuffle)[0]
+                    # You won!
+                    @game_over = true
+                end
+                @board[row][col] = true
+                @last_move = [row, col]
                 @thinking = false
             end
         end
@@ -81,7 +109,11 @@ class Window < Gosu::Window
                 draw_rect(col*@cell_size, row*@cell_size, @cell_size, @cell_size, @col_white)
                 draw_rect(col*@cell_size+1, row*@cell_size+1, @cell_size-2, @cell_size-2, @col_black)
                 if @board[row][col]
-                    @circle.draw(col*@cell_size, row*@cell_size, 0, 0.5, 0.5, @col_red)
+                    if @last_move[0] == row and @last_move[1] == col
+                        @circle.draw(col*@cell_size+5, row*@cell_size+5, 0, 0.4, 0.4, @col_red)
+                    else
+                        @circle.draw(col*@cell_size+5, row*@cell_size+5, 0, 0.4, 0.4, @col_green)
+                    end
                 end
             }
         }
@@ -103,6 +135,7 @@ class Window < Gosu::Window
                 row = mouse_y / @cell_size
                 if row >= 0 and row < @size and col >= 0 and col < @size
                     @board[row][col] = 1
+                    @last_move = [row, col]
                     @thinking = true
                 end
             end
