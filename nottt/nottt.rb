467cc8359d634276e5d08e81a141b23300f67890
Michael Jørgensen <michael.finn.jorgensen@gmail.com>
2017-02-26 22:39:16 +0100

    Checkin time.

--- nottt/nottt.rb
+++ nottt/nottt.rb
@@ -9,6 +9,7 @@ class Window < Gosu::Window
         @col_black = Gosu::Color.new(0xff000000)
         @col_red   = Gosu::Color.new(0xffff8080)
         @col_green = Gosu::Color.new(0xff80ff80)
+        @col_blue  = Gosu::Color.new(0xff8080ff)
 
         @cell_size = 50
         @size = 4
@@ -19,8 +20,33 @@ class Window < Gosu::Window
        
         @thinking = false
         @game_over = false
+        @help = false
         
         @last_move = [0, 0]
+        @line = []
+
+        # Build list of lines
+        @lines = []
+        (0..@size-1).each {|row|
+            (0..@size-3).each {|col|
+                @lines << [[row, col], [row, col+1], [row, col+2]]
+            }
+        }
+        (0..@size-3).each {|row|
+            (0..@size-1).each {|col|
+                @lines << [[row, col], [row+1, col], [row+2, col]]
+            }
+        }
+        (0..@size-3).each {|row|
+            (0..@size-3).each {|col|
+                @lines << [[row, col], [row+1, col+1], [row+2, col+2]]
+            }
+        }
+        (2..@size-1).each {|row|
+            (0..@size-3).each {|col|
+                @lines << [[row, col], [row-1, col+1], [row-2, col+2]]
+            }
+        }
     end
 
     def needs_cursor?
@@ -28,39 +54,37 @@ class Window < Gosu::Window
     end
 
     def is_dead?
-        (0..@size-1).each {|row|
-            (0..@size-3).each {|col|
-                if @board[row][col] and @board[row][col+1] and @board[row][col+2]
-                    return true
-                end
-            }
-        }
-
-        (0..@size-3).each {|row|
-            (0..@size-1).each {|col|
-                if @board[row][col] and @board[row+1][col] and @board[row+2][col]
-                    return true
-                end
-            }
-        }
-
-        (0..@size-3).each {|row|
-            (0..@size-3).each {|col|
-                if @board[row][col] and @board[row+1][col+1] and @board[row+2][col+2]
-                    return true
-                end
-            }
-        }
-
-        (2..@size-1).each {|row|
-            (0..@size-3).each {|col|
-                if @board[row][col] and @board[row-1][col+1] and @board[row-2][col+2]
-                    return true
-                end
-            }
+        @lines.each {|line|
+            if @board[line[0][0]][line[0][1]] and 
+                    @board[line[1][0]][line[1][1]] and
+                    @board[line[2][0]][line[2][1]]
+                @line = line
+                return true
+            end
         }
 
         return false
+    end
+
+    def get_line_counts(sq)
+        vals = [0, 0, 0, 0]
+        @lines.each {|line|
+            if line.index(sq)
+                count = 0
+                if @board[line[0][0]][line[0][1]]
+                    count += 1
+                end
+                if @board[line[1][0]][line[1][1]]
+                    count += 1
+                end
+                if @board[line[2][0]][line[2][1]]
+                    count += 1
+                end
+                # count is now between 0 and 3.
+                vals[count] += 1
+            end
+        }
+        return vals
     end
 
     def update
@@ -70,31 +94,42 @@ class Window < Gosu::Window
                 # We won!
                 @game_over = true
             else
-                # Look through all empty square
+                # Look through all empty squares
                 moves = []
                 legal = []
+                min_val = 10
                 (0..@size-1).each {|row|
                     (0..@size-1).each {|col|
                         if not @board[row][col]
-                            legal << [row, col]
-                            @board[row][col] = true
-                            if not is_dead?
-                                moves << [row, col]
+                            # This square is empty
+                            sq = [row, col]
+                            legal << sq
+                            vals = get_line_counts(sq)
+                            if vals[2] == 0
+                                if vals[1] <= min_val
+                                    if vals[1] < min_val
+                                        moves = []
+                                        min_val = vals[1]
+                                    end
+                                    moves << sq
+                                end
                             end
-                            @board[row][col] = false
                         end
                     }
                 }
-                if moves.size > 0
+                if moves.size > 0 # Any good moves?
                     row, col = (moves.shuffle)[0]
-                else
+                else # OK, just choose a legal move.
                     row, col = (legal.shuffle)[0]
-                    # You won!
-                    @game_over = true
                 end
                 @board[row][col] = true
                 @last_move = [row, col]
                 @thinking = false
+
+                if is_dead?
+                    # You won!
+                    @game_over = true
+                end
             end
         end
     end
@@ -104,6 +139,9 @@ class Window < Gosu::Window
     end
 
     def draw
+        @line = []
+        is_dead?
+        index = [0]*10
         (0..@size-1).each {|row|
             (0..@size-1).each {|col|
                 draw_rect(col*@cell_size, row*@cell_size, @cell_size, @cell_size, @col_white)
@@ -111,12 +149,45 @@ class Window < Gosu::Window
                 if @board[row][col]
                     if @last_move[0] == row and @last_move[1] == col
                         @circle.draw(col*@cell_size+5, row*@cell_size+5, 0, 0.4, 0.4, @col_red)
+                    elsif @line.index([row, col])
+                        @circle.draw(col*@cell_size+5, row*@cell_size+5, 0, 0.4, 0.4, @col_blue)
                     else
                         @circle.draw(col*@cell_size+5, row*@cell_size+5, 0, 0.4, 0.4, @col_green)
                     end
                 end
+                if @help
+                    vals = get_line_counts([row, col])
+                    if vals[2] > 0
+                        index[0] += 1
+                    elsif vals[1] >= 8
+                        index[1] += 1
+                    elsif vals[1] == 7
+                        index[2] += 1
+                    elsif vals[1] == 6
+                        index[3] += 1
+                    elsif vals[1] == 5
+                        index[4] += 1
+                    elsif vals[1] == 4
+                        index[5] += 1
+                    elsif vals[1] == 3
+                        index[6] += 1
+                    elsif vals[1] == 2
+                        index[7] += 1
+                    elsif vals[1] == 1
+                        index[8] += 1
+                    elsif vals[0] > 0
+                        index[9] += 1
+                    end
+
+                    if vals[2] == 0
+                        @font.draw("#{vals[1]}", col*@cell_size+20, row*@cell_size+20, 0)
+                    end
+                end
             }
         }
+        if @help
+            @font.draw("#{index}", 0, @size*@cell_size+20, 0)
+        end
 
         if @game_over
             @font.draw("Game over!", @size*@cell_size+10, 0, 0);
@@ -129,6 +200,8 @@ class Window < Gosu::Window
         case id
         when Gosu::KbEscape
             close
+        when Gosu::KbH
+            @help = !@help
         when Gosu::MsLeft
             if not @thinking
                 col = mouse_x / @cell_size
