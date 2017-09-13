d722836e417f425553abb6b24b42032b1f433866
Michael Jørgensen <michael.finn.jorgensen@gmail.com>
2017-09-13 17:14:11 +0200

    Checkin time.

--- CookieClicker/main.rb
+++ CookieClicker/main.rb
@@ -1,29 +1,23 @@
 #!/bin/env ruby
 
-# This adds the "gosu" library to the game, which enables various extra
-# features!
 require 'gosu'
 
-# These are nessecary for THIS code/file to cooperate with the other
-# files/codes
 require_relative 'cookie.rb'
 require_relative 'unit.rb'
 
-# This adds the actual window to the game, which in this case works as a
-# controller!
 class GameWindow < Gosu::Window
-    attr_reader :player, :score
+    attr_reader :font
 
-    # This is the event that occurs when you start the game, like when the object
-    # is "created"
     def initialize
         super(940, 600, false)  # Set size of window
+        @font   = Gosu::Font.new(self, Gosu.default_font_name, 24)
 
         @cookie = Cookie.new(100, 100, "media/PerfectCookie.png")
 
         @units = []
-        @units.push(Unit.new(0, 0, "media/CursorIconTransparent.png", "Cursor", 15, 0.1))
-
+        @units.push(Unit.new(self, 400, 50,  "media/CursorIconTransparent.png", "Cursor",   15.0,   0.1))
+        @units.push(Unit.new(self, 400, 120, "media/AlienGrandma.png",          "Grandma", 100.0,   1))
+        @units.push(Unit.new(self, 400, 190, "media/FarmIconTransparent.png",   "Farm",      1.1e3, 8))
     end
 
     def needs_cursor?
@@ -32,20 +26,24 @@ class GameWindow < Gosu::Window
 
     # This event is checked 60 times per second.
     def update
-        self.caption = "Cookie Clicker - [Cookies: #{@cookie.cookies.to_i}, CPS: #{0}]"
-
+        totalCps = 0
         @units.each { |inst|
+            totalCps += inst.cps * inst.number
             @cookie.increase(inst.cps * inst.number / 60.0)
         }
+        self.caption = "Cookie Clicker - [Cookies: #{@cookie.cookies.to_i}, CPS: #{totalCps.round(1)}]"
     end
 
     def mouse(x, y)
+        # Have we clicked the cookie?
         if @cookie.in_range?(x, y)
             @cookie.increase(1)
         end
+
+        # Have we clicked on a unit?
         @units.each { |inst| 
             if inst.in_range?(x, y)
-                if @cookie.cookies >= inst.cost
+                if @cookie.cookies >= inst.cost # Can we afford to buy the unit?
                     @cookie.increase(-inst.cost)
                     inst.buy
                 end
--- CookieClicker/unit.rb
+++ CookieClicker/unit.rb
@@ -1,10 +1,11 @@
 class Unit
-    attr_reader :cost, :number, :cps
+    attr_reader :number, :cps
 
-    def initialize(x, y, img_file, name, cost, cps)
-        @x, @y, @img_file, @name, @cost, @cps = x, y, img_file, name, cost, cps
+    def initialize(window, x, y, img_file, name, cost, cps)
+        @window, @x, @y, @img_file, @name, @cost, @cps = window, x, y, img_file, name, cost, cps
 
         @image  = Gosu::Image.new(@img_file)
+        @scale  = 50.0 / @image.height
         @number = 0
     end
 
@@ -13,13 +14,20 @@ class Unit
         @cost *= 1.15
     end
 
+    def cost
+        @cost.ceil
+    end
+
     # Check if (x,y) is within the image of the unit
     def in_range?(x, y)
-        (x >= @x and x < @x + @image.width and
-         y >= @y and y < @y + @image.height)
+        (x >= @x and x < @x + @image.width*@scale and
+         y >= @y and y < @y + @image.height*@scale)
     end
 
     def draw
-        @image.draw(@x, @y, 0)
+        @image.draw(@x, @y, 0, @scale, @scale)
+        @window.font.draw(@name, @x+100, @y+20, 0)
+        @window.font.draw("#{cost}", @x+200, @y+20, 0)
+        @window.font.draw("#{(cost/@cps).ceil}", @x+300, @y+20, 0)
     end
 end
