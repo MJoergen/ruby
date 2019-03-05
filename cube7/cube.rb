5c3be33507bfe2252fc567970a83a7e532be044e
Michael Jørgensen <mfj@silicom.dk>
2019-03-05 13:09:26 +0100

    Added corner checks.

--- cube7/cube.rb
+++ cube7/cube.rb
@@ -26,84 +26,95 @@ class Cube
 
       # 24 edge pieces close to the corner
       @edges1 = []
-      @edges1 << [0, 1, 0, 1, 1, 6]
-      @edges1 << [0, 0, 5, 2, 6, 5]
-      @edges1 << [0, 6, 1, 3, 0, 1]
-      @edges1 << [0, 5, 6, 4, 5, 0]
-      @edges1 << [2, 1, 0, 1, 0, 1]
-      @edges1 << [3, 1, 0, 1, 6, 5]
-      @edges1 << [5, 1, 0, 1, 5, 0]
-      @edges1 << [2, 5, 6, 4, 0, 1]
-      @edges1 << [5, 6, 1, 2, 0, 1]
-      @edges1 << [4, 6, 1, 3, 1, 6]
-      @edges1 << [5, 0, 5, 3, 6, 5]
-      @edges1 << [5, 5, 6, 4, 1, 6]
-      @edges1 << [1, 5, 6, 0, 5, 0]
-      @edges1 << [2, 6, 1, 0, 0, 1]
-      @edges1 << [3, 0, 5, 0, 6, 5]
-      @edges1 << [4, 1, 0, 0, 1, 6]
-      @edges1 << [1, 0, 5, 2, 5, 0]
-      @edges1 << [1, 6, 1, 3, 5, 0]
-      @edges1 << [1, 1, 0, 5, 5, 0]
-      @edges1 << [4, 0, 5, 2, 1, 6]
-      @edges1 << [2, 0, 5, 5, 6, 5]
-      @edges1 << [3, 5, 6, 4, 6, 5]
-      @edges1 << [3, 6, 1, 5, 0, 1]
-      @edges1 << [4, 5, 6, 5, 1, 6]
+      @edges1 << [0, 1, 0,  1, 1, 6]
+      @edges1 << [0, 0, 5,  2, 6, 5]
+      @edges1 << [0, 6, 1,  3, 0, 1]
+      @edges1 << [0, 5, 6,  4, 5, 0]
+      @edges1 << [2, 1, 0,  1, 0, 1]
+      @edges1 << [3, 1, 0,  1, 6, 5]
+      @edges1 << [5, 1, 0,  1, 5, 0]
+      @edges1 << [2, 5, 6,  4, 0, 1]
+      @edges1 << [5, 6, 1,  2, 0, 1]
+      @edges1 << [4, 6, 1,  3, 1, 6]
+      @edges1 << [5, 0, 5,  3, 6, 5]
+      @edges1 << [5, 5, 6,  4, 1, 6]
+      @edges1 << [1, 5, 6,  0, 5, 0]
+      @edges1 << [2, 6, 1,  0, 0, 1]
+      @edges1 << [3, 0, 5,  0, 6, 5]
+      @edges1 << [4, 1, 0,  0, 1, 6]
+      @edges1 << [1, 0, 5,  2, 5, 0]
+      @edges1 << [1, 6, 1,  3, 5, 0]
+      @edges1 << [1, 1, 0,  5, 5, 0]
+      @edges1 << [4, 0, 5,  2, 1, 6]
+      @edges1 << [2, 0, 5,  5, 6, 5]
+      @edges1 << [3, 5, 6,  4, 6, 5]
+      @edges1 << [3, 6, 1,  5, 0, 1]
+      @edges1 << [4, 5, 6,  5, 1, 6]
 
       # 24 edge pieces close to the centre
       @edges2 = []
-      @edges2 << [0, 2, 0, 1, 2, 6]
-      @edges2 << [0, 0, 4, 2, 6, 4]
-      @edges2 << [0, 6, 2, 3, 0, 2]
-      @edges2 << [0, 4, 6, 4, 4, 0]
-      @edges2 << [2, 2, 0, 1, 0, 2]
-      @edges2 << [3, 2, 0, 1, 6, 4]
-      @edges2 << [5, 2, 0, 1, 4, 0]
-      @edges2 << [2, 4, 6, 4, 0, 2]
-      @edges2 << [5, 6, 2, 2, 0, 2]
-      @edges2 << [4, 6, 2, 3, 2, 6]
-      @edges2 << [5, 0, 4, 3, 6, 4]
-      @edges2 << [5, 4, 6, 4, 2, 6]
-      @edges2 << [1, 4, 6, 0, 5, 0]
-      @edges2 << [2, 6, 2, 0, 0, 2]
-      @edges2 << [3, 0, 4, 0, 6, 4]
-      @edges2 << [4, 2, 0, 0, 2, 6]
-      @edges2 << [1, 0, 4, 2, 4, 0]
-      @edges2 << [1, 6, 2, 3, 4, 0]
-      @edges2 << [1, 2, 0, 5, 4, 0]
-      @edges2 << [4, 0, 4, 2, 2, 6]
-      @edges2 << [2, 0, 4, 5, 6, 4]
-      @edges2 << [3, 4, 6, 4, 6, 4]
-      @edges2 << [3, 6, 2, 5, 0, 2]
-      @edges2 << [4, 4, 6, 5, 2, 6]
+      @edges2 << [0, 2, 0,  1, 2, 6]
+      @edges2 << [0, 0, 4,  2, 6, 4]
+      @edges2 << [0, 6, 2,  3, 0, 2]
+      @edges2 << [0, 4, 6,  4, 4, 0]
+      @edges2 << [2, 2, 0,  1, 0, 2]
+      @edges2 << [3, 2, 0,  1, 6, 4]
+      @edges2 << [5, 2, 0,  1, 4, 0]
+      @edges2 << [2, 4, 6,  4, 0, 2]
+      @edges2 << [5, 6, 2,  2, 0, 2]
+      @edges2 << [4, 6, 2,  3, 2, 6]
+      @edges2 << [5, 0, 4,  3, 6, 4]
+      @edges2 << [5, 4, 6,  4, 2, 6]
+      @edges2 << [1, 4, 6,  0, 5, 0]
+      @edges2 << [2, 6, 2,  0, 0, 2]
+      @edges2 << [3, 0, 4,  0, 6, 4]
+      @edges2 << [4, 2, 0,  0, 2, 6]
+      @edges2 << [1, 0, 4,  2, 4, 0]
+      @edges2 << [1, 6, 2,  3, 4, 0]
+      @edges2 << [1, 2, 0,  5, 4, 0]
+      @edges2 << [4, 0, 4,  2, 2, 6]
+      @edges2 << [2, 0, 4,  5, 6, 4]
+      @edges2 << [3, 4, 6,  4, 6, 4]
+      @edges2 << [3, 6, 2,  5, 0, 2]
+      @edges2 << [4, 4, 6,  5, 2, 6]
 
       # 12 edge pieces at the centre (and swapped)
       @edges3 = []
-      @edges3 << [0, 3, 0, 1, 3, 6]
-      @edges3 << [0, 0, 3, 2, 6, 3]
-      @edges3 << [0, 6, 3, 3, 0, 3]
-      @edges3 << [0, 3, 6, 4, 3, 0]
-      @edges3 << [2, 3, 0, 1, 0, 3]
-      @edges3 << [3, 3, 0, 1, 6, 3]
-      @edges3 << [5, 3, 0, 1, 3, 0]
-      @edges3 << [2, 3, 6, 4, 0, 3]
-      @edges3 << [5, 6, 3, 2, 0, 3]
-      @edges3 << [4, 6, 3, 3, 3, 6]
-      @edges3 << [5, 0, 3, 3, 6, 3]
-      @edges3 << [5, 3, 6, 4, 3, 6]
-      @edges3 << [1, 3, 6, 0, 3, 0]
-      @edges3 << [2, 6, 3, 0, 0, 3]
-      @edges3 << [3, 0, 3, 0, 6, 3]
-      @edges3 << [4, 3, 0, 0, 3, 6]
-      @edges3 << [1, 0, 3, 2, 3, 0]
-      @edges3 << [1, 6, 3, 3, 3, 0]
-      @edges3 << [1, 3, 0, 5, 3, 0]
-      @edges3 << [4, 0, 3, 2, 3, 6]
-      @edges3 << [2, 0, 3, 5, 6, 3]
-      @edges3 << [3, 3, 6, 4, 6, 3]
-      @edges3 << [3, 6, 3, 5, 0, 3]
-      @edges3 << [4, 3, 6, 5, 3, 6]
+      @edges3 << [0, 3, 0,  1, 3, 6]
+      @edges3 << [0, 0, 3,  2, 6, 3]
+      @edges3 << [0, 6, 3,  3, 0, 3]
+      @edges3 << [0, 3, 6,  4, 3, 0]
+      @edges3 << [2, 3, 0,  1, 0, 3]
+      @edges3 << [3, 3, 0,  1, 6, 3]
+      @edges3 << [5, 3, 0,  1, 3, 0]
+      @edges3 << [2, 3, 6,  4, 0, 3]
+      @edges3 << [5, 6, 3,  2, 0, 3]
+      @edges3 << [4, 6, 3,  3, 3, 6]
+      @edges3 << [5, 0, 3,  3, 6, 3]
+      @edges3 << [5, 3, 6,  4, 3, 6]
+      @edges3 << [1, 3, 6,  0, 3, 0]
+      @edges3 << [2, 6, 3,  0, 0, 3]
+      @edges3 << [3, 0, 3,  0, 6, 3]
+      @edges3 << [4, 3, 0,  0, 3, 6]
+      @edges3 << [1, 0, 3,  2, 3, 0]
+      @edges3 << [1, 6, 3,  3, 3, 0]
+      @edges3 << [1, 3, 0,  5, 3, 0]
+      @edges3 << [4, 0, 3,  2, 3, 6]
+      @edges3 << [2, 0, 3,  5, 6, 3]
+      @edges3 << [3, 3, 6,  4, 6, 3]
+      @edges3 << [3, 6, 3,  5, 0, 3]
+      @edges3 << [4, 3, 6,  5, 3, 6]
+
+      # 8 corner pieces
+      @corners = []
+      @corners << [0, 6, 0,  3, 0, 0,  1, 6, 6]
+      @corners << [0, 0, 0,  1, 0, 6,  2, 6, 0]
+      @corners << [0, 0, 6,  2, 6, 6,  4, 0, 0]
+      @corners << [0, 6, 6,  4, 6, 0,  3, 0, 0]
+      @corners << [5, 6, 0,  1, 6, 6,  3, 0, 0]
+      @corners << [5, 0, 0,  2, 6, 0,  1, 0, 6]
+      @corners << [5, 0, 6,  4, 0, 0,  2, 6, 6]
+      @corners << [5, 6, 6,  3, 0, 0,  4, 6, 0]
    end
 
    def draw
@@ -137,8 +148,8 @@ class Cube
             for j in i..3
                ypos += 40
                count = get_colour_count(c, i, j)
-               @font.draw("#{count}", xpos + 30*c, ypos, 2, 1.0, 1.0,
-                          Gosu::Color.argb(0xff_ffffff))
+               @font.draw_text("#{count}", xpos + 30*c, ypos, 2, 1.0, 1.0,
+                               Gosu::Color.argb(0xff_ffffff))
             end
          end
       end
@@ -190,6 +201,31 @@ class Cube
       return errors
    end
 
+   def check_corners
+      errors = 0
+      exp = [0]*216
+      obs = [0]*216
+      for c in 0..7
+         corner = @corners[c]
+         col1 = @faces[corner[0]].get_col(corner[1], corner[2])
+         col2 = @faces[corner[3]].get_col(corner[4], corner[5])
+         col3 = @faces[corner[6]].get_col(corner[7], corner[8])
+         exp_colour_pair = 36*corner[0] + 6*corner[3] + corner[6]
+         exp[exp_colour_pair] += 1
+         if col1 < 6 and col2 < 6 and col3 < 6
+            colour_pair = 36*col1 + 6*col2 + col3
+            obs[colour_pair] += 1
+         end
+      end
+      for c in 0..215
+         if obs[c] > exp[c]
+            errors += 1
+         end
+      end
+
+      return errors
+   end
+
    def legal?
       errors = 0
       for i in 0..2
@@ -201,6 +237,8 @@ class Cube
       errors += check_edges(@edges1)
       errors += check_edges(@edges2)
       errors += check_edges(@edges3)
+
+      errors += check_corners
 
       return (errors == 0)
    end
